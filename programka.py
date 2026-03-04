12e6ae92212b0cb645c79fe2ede86939ec234b45
Sava5713-IT <sava57135713@outlook.com>
2026-03-04 20:04:43 +0300

    Add files via upload

---
+++ programka.py
@@ -0,0 +1,17 @@
+a = 111
+b = 3.5
+c = 222
+d = False
+e = True
+g = [1,2,3]
+print(a)
+print(a + b)
+print(a + e)
+print(type(a))
+print(type(b))
+print(type(type(d)))
+type(g)
+print(b * c)
+print(d)
+print(a * d)
+
